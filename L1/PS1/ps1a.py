a005c431ddd4b099f5d64d5d643570a8a9db185a
notjdr <jacektomek21@gmail.com>
2024-04-24 23:25:24 +0200

    experiments in folder.

--- L1/PS1/ps1a.py
+++ L1/PS1/ps1a.py
@@ -102,7 +102,8 @@ def brute_force_cow_transport(cows,limit=10):
     Finds the allocation of cows that minimizes the number of spaceship trips
     via brute force.  The brute force algorithm should follow the following method:
 
-    1. Enumerate all possible ways that the cows can be divided into separate trips Use the given get_partitions function in ps1_partition.py to help you!  2. Select the allocation that minimizes the number of trips without making any trip that does not obey the weight limitation Does not mutate the given dictionary of cows.
+    1. Enumerate all possible ways that the cows can be divided into separate trips Use the given get_partitions function in ps1_partition.py to help you!  
+    2. Select the allocation that minimizes the number of trips without making any trip that does not obey the weight limitation Does not mutate the given dictionary of cows.
     Parameters:
     cows - a dictionary of name (string), weight (int) pairs
     limit - weight limit of the spaceship (an int)
